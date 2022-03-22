1ce199e73c616c64193aa9e8298633948f3e7d54
Alfaro110399 <edudeleon99@gmail.com>
2022-03-21 19:28:56 -0600

    Hola
    buenas

--- Registro_Familiar/Registro_Familiar/Form1.cs
+++ Registro_Familiar/Registro_Familiar/Form1.cs
@@ -16,5 +16,10 @@ namespace Registro_Familiar
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
